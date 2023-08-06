f68136099bf24615a4c7a14fbb5359700f3d8154
ouyangfeng777 <141560492+ouyangfeng777@users.noreply.github.com>
2023-08-06 21:22:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,7 +8,7 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: ' wx13b794f4aea50849',
+  APP_ID: 'wx13b794f4aea50849',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '8695fb537b958e717a468e36cdf965ed',
